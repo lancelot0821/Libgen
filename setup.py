703baeb0f0326bd1cc639616e6ffed0b22bdd838
//Fanzhou Kong <36810889+lancelot0821@users.noreply.github.com>
2022-04-01 12:17:42 -0700

    test for functionality
    Data spell demo

--- setup.py
+++ setup.py
@@ -9,8 +9,10 @@ def set_workspace(workplace):
         os.makedirs(os.path.join(workplace, 'sirius_workspace','opt'))
         os.makedirs(os.path.join(workplace, 'temp_data'))
         os.makedirs(os.path.join(workplace, 'temp_data','msms'))
+        os.chdir(workplace)
     else:
         print("the workspace exists!")
+        os.chdir(workplace)
 
 
 print("i am setup functions!!")
